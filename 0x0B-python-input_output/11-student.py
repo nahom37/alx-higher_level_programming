d15946e127ea39166a67739a5383bd4778e51776
redieight <radietalx@gmail.com>
2021-12-21 22:44:05 +0300

    update

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -12,8 +12,24 @@ class Student:
         self.last_name = last_name
         self.age = age
 
-    def to_json(self):
-        """Retrieves dictionary of Student
+    def to_json(self, attrs=None):
+        """Retrieves dictionary of Student with conditions to filter
         """
 
-        return self.__dict__
+        if attrs == None or type(attrs) != list:
+            return self.__dict__
+        else:
+            temp = {}
+            for elem in attrs:
+                if type(elem) != str:
+                    return self.__dict__
+                if elem in self.__dict__.keys():
+                    temp[elem] = self.__dict__[elem]
+            return temp
+
+    def reload_from_json(self, json):
+        """Replaces all items in `json`
+        """
+
+        for items in json.keys():
+            self.__dict__[items] = json[items]
